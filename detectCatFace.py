c3b8f99189f1dd4010653dd6c50d3eaca40c6009
Alonso_Flo14 <florian.alonso.henallux@gmail.com>
2020-03-20 17:01:43 +0100

    Function to extract cat's face and result display added

--- detectCatFace.py
+++ detectCatFace.py
@@ -16,6 +16,20 @@ def detectCatFace(img):
         cv2.rectangle(cat_img, (x,y), (x+w,y+h), (255,255,255), 10) 
     return cat_img
 
+#function to extract cat's face    
+def recoverCatFace(img):
+    cat_img = img.copy()
+    cat_rects = cat_face_cascade.detectMultiScale(cat_img)
+    cat_face = ()
+    if cat_rects != ():
+        for (x,y,w,h) in cat_rects: 
+            cat_face = cat_img[y:y+h,x:x+w]
+    return cat_face	
+
 #call the function and show the result     
 result = detectCatFace(cat)
 plt.imshow(result,cmap='gray')
+
+#call the extract function and show the result
+face = recoverCatFace(cat)
+plt.imshow(face,cmap='gray')
