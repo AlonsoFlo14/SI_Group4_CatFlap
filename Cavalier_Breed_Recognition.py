1cb0c17c3898a5dcd5129853ca35f1d19b9dac55
Alonso_Flo14 <florian.alonso.henallux@gmail.com>
2020-05-05 03:49:40 +0200

    Adding some lines in the training code to display where we are (code progression) and suppression de la partie détection.

--- Cavalier_Breed_Recognition.py
+++ Cavalier_Breed_Recognition.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import os
 import re
 import h5py
@@ -14,6 +8,7 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import tensorflow
 from tensorflow.keras import regularizers
 from tensorflow.keras.layers import GlobalAveragePooling2D,Dense
 from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
@@ -26,16 +21,10 @@ from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
 from sklearn.preprocessing import MinMaxScaler,LabelEncoder
 from sklearn.model_selection import train_test_split
 
-
-# In[2]:
-
+print('Librairies Imported...')
 
 img_height = 224
 img_width = 224
-
-
-# In[3]:
-
 
 def generate_unique_logpath(logdir, raw_run_name):
     i = 0
@@ -45,10 +34,6 @@ def generate_unique_logpath(logdir, raw_run_name):
         if not os.path.isdir(log_path):
             return log_path
         i = i + 1
-
-
-# In[4]:
-
 
 def path_to_tensor(img_path):
     """Takes path to single image and returns it as a 4D tensor"""
@@ -63,43 +48,39 @@ def paths_to_tensor(img_paths):
     list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
     return np.vstack(list_of_tensors)
 
-
-# In[5]:
-
-
 def extract_Resnet50(tensor):
     bottleneck_feature = ResNet50(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
     return bottleneck_feature
-
-
-# In[6]:
-
+    
+def Resnet50_predict_breed(img_path):
+    """Takes image path, formats it into 4D tensor , returns name of dog corresponding to ResNet50's prediction"""
+    # extract bottleneck features
+    bottleneck_feature = extract_Resnet50(path_to_tensor(img_path))
+    # obtain predicted vector
+    predicted_vector = Resnet50_model.predict(bottleneck_feature)
+    # return dog breed that is predicted by the model
+    return train_targets[np.argmax(predicted_vector)]
 
 cavalier_dir = "../dogFlap/dataset/cavalier/"
 unknow_dir = "../dogFlap/dataset/unknow/"
+print('Dataset initialized')
+print('The cavalier directory is : ', cavalier_dir)
+print('The unknow directory is : ', unknow_dir)
 cavalier_list = [cavalier_dir+f for f in os.listdir(cavalier_dir) if re.search('jpg|JPG', f)]
 unknow_list = [unknow_dir+f for f in os.listdir(unknow_dir) if re.search('jpg|JPG', f)]
 train_list = cavalier_list + unknow_list
 train_targets = ["cavalier","unknow"]
 train_targets_list = ["cavalier" for i in range(len(cavalier_list))] + ["unknow" for i in range(len(unknow_list))]
 
-
-# In[7]:
-
+print('Get name of all images...')
 
 train_tensors = paths_to_tensor(train_list)
 del  train_list
 
-
-# In[8]:
-
+print('Transformed all images into tensors...')
 
 X_train, X_test, y_train, y_test = train_test_split(train_tensors,train_targets_list,test_size=0.20,random_state=42)
 del train_tensors, train_targets_list
-
-
-# In[9]:
-
 
 y_train = np.array(y_train)
 label_encoder_train = LabelEncoder()
@@ -112,18 +93,16 @@ y_test = label_encoder_test.fit_transform(y_test)
 y_test = to_categorical(y_test)
 print(y_test.shape[0])
 
-
-# In[10]:
-
+print('About to extract ResNet50 features...')
 
 train_Resnet50 = extract_Resnet50(X_train)
 del X_train
 test_Resnet50 = extract_Resnet50(X_test)
 del X_test
 
+print('Input shape for the CNN : ',train_Resnet50.shape[1:])
 
-# In[11]:
-
+print('Extraction completed...')
 
 Resnet50_model = Sequential()
 Resnet50_model.add(GlobalAveragePooling2D(input_shape=train_Resnet50.shape[1:]))
@@ -131,16 +110,8 @@ Resnet50_model.add(Dense(2, activation='softmax'))
 
 Resnet50_model.summary()
 
-
-# In[12]:
-
-
 sgd = SGD(lr=0.01, clipnorm=1, decay=1e-6, momentum = 0.9, nesterov=True)
 Resnet50_model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-
-
-# In[13]:
-
 
 run_name = "linear"
 logpath = generate_unique_logpath(".\logs_cavalier", run_name)
@@ -149,9 +120,7 @@ tbcb = TensorBoard(log_dir=logpath)
 checkpoint_filepath = os.path.join(logpath,  "best_Resnet50_model.h5")
 checkpoint_cb = ModelCheckpoint(checkpoint_filepath, save_best_only=True)
 
-
-# In[14]:
-
+print('Training the model...')
 
 Resnet50_model.fit(train_Resnet50, y_train,
           batch_size=128,
@@ -160,50 +129,16 @@ Resnet50_model.fit(train_Resnet50, y_train,
           validation_split=0.1,
           callbacks=[tbcb,checkpoint_cb])
 
-
-# In[15]:
-
+print('Training completed...')
 
 score = Resnet50_model.evaluate(test_Resnet50, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 
+# Resnet50_model.load_weights(logpath + '/best_Resnet50_model.h5')
 
-# In[17]:
+# img_path = "../dogFlap/cavalier.jpg"
+# dog = cv2.imread(img_path,0)
+# plt.imshow(dog,cmap='gray')
 
-
-Resnet50_model.load_weights(logpath + '/best_Resnet50_model.h5')
-
-
-# In[18]:
-
-
-img_path = "../dogFlap/cavalier.jpg"
-dog = cv2.imread(img_path,0)
-plt.imshow(dog,cmap='gray')
-
-
-# In[19]:
-
-
-def Resnet50_predict_breed(img_path):
-    """Takes image path, formats it into 4D tensor , returns name of dog corresponding to ResNet50's prediction"""
-    # extract bottleneck features
-    bottleneck_feature = extract_Resnet50(path_to_tensor(img_path))
-    # obtain predicted vector
-    predicted_vector = Resnet50_model.predict(bottleneck_feature)
-    # return dog breed that is predicted by the model
-    return train_targets[np.argmax(predicted_vector)]
-
-
-# In[20]:
-
-
-Resnet50_predict_breed(img_path)
-
-
-# In[ ]:
-
-
-
-
+# Resnet50_predict_breed(img_path)
